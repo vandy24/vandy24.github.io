e5ecd48942a501fb749117846e01dacd6b8b10fa
vandy123 <vandy123@umn.edu>
2019-12-13 11:34:44 -0600

    a

--- app.py
+++ app.py
@@ -2,45 +2,74 @@
 docstring
 """
 from flask import Flask, render_template, request, g
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bmiller:@localhost:5432/world'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///world.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///final.db'
 db = SQLAlchemy(app)
+email = None
+password = None
+
+class LoginForm(FlaskForm):
+    email = StringField("Email")
+    password = StringField("Password")
+    submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email")
+    password = StringField("Password")
+    submit = SubmitField("Register")
+    
+csrf = CSRFProtect()
+app.config["SECRET_KEY"] = "row the boat"
+csrf.init_app(app)
 
 
 @app.route('/')
-def all_countries():
-    res = db.session.execute('select * from country')
-    c_list = res.fetchall()
-    cols = res.keys()
-    print(c_list[0]['name'])
-    curs = db.session.execute('select distinct continent from country')
-    cont_list = curs.fetchall()
+def login():
+    global email
+    global password
+    form = LoginForm()
+    reg = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email
+        password = form.password
+        res = db.session.execute("select email from user")
+        emails = res.fetchall()
+        res = db.session.execute("select password from user")
+        passwords = res.fetchall()
+        if not (email in email and password in passwords):
+            return render_template("login.html", form=form)
+    else: return render_template("login.html", form=form)
+    
+    if reg.validate_on_submit():
+        email = reg.email
+        password = reg.password
+        res = db.session.execute("select email from user")
+        emails = res.fetchall()
+        if not (email in email):
+            db.session.execute(user.insert(), email=email, password=password)
+    else: return render_template("login.html", form=form)
 
-    return render_template('country_boot.html',
-                           countries=c_list,
-                           continents=cont_list,
-                           columns=cols)
+##@app.route('/index')
+##def citylist(c_code):
+##    res = db.session.execute('select * from city where countrycode = :ccode',
+##                             {'ccode': c_code})
+##    return render_template('cities.html', cities=res.fetchall(),
+##                           columns=res.keys())
 
-@app.route('/citylist/<c_code>')
-def citylist(c_code):
-    res = db.session.execute('select * from city where countrycode = :ccode',
-                             {'ccode': c_code})
-    return render_template('cities.html', cities=res.fetchall(),
-                           columns=res.keys())
-
-@app.route('/continent')
-def countries_by_continent():
-    continent = request.args['selected_continent']
-    res = db.session.execute('select * from country where continent = :cont order by name',
-                             {'cont': continent})
-    c_list = res.fetchall()
-
-    return render_template('country_boot.html',
-                           countries=c_list,
-                           continents=[],
-                           columns=res.keys())
+##@app.route('/continent')
+##@login_required
+##def countries_by_continent():
+##    continent = request.args['selected_continent']
+##    res = db.session.execute('select * from country where continent = :cont order by name',
+##                             {'cont': continent})
+##    c_list = res.fetchall()
+##
+##    return render_template('country_boot.html',
+##                           countries=c_list,
+##                           continents=[],
+##                           columns=res.keys())
